7a71d445b3aa380a2a3f92e2e963efa8c29b2dde
eldiaby <elgdiab@gmail.com>
2024-09-12 19:21:42 +0300

    working in the 13th section

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -6,6 +6,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -31,7 +33,60 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+///////////////////////////////////////
+// Button scrolling
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+///////////////////////////////////////////////
+// page navigation
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = el.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+///////////////////////////////////////////////////////
+// lecrtures
 /*
+
+
 // cookie message
 const message = document.createElement('div');
 message.classList.add('cookie-message');
@@ -40,10 +95,9 @@ document.querySelector('header').append(message);
 document.querySelector('.btn--close-cookie').addEventListener('click', () => {
   message.remove();
 });
-*/
 
-///////////////////////////////////////////////////////
-// lecrtures
+
+
 
 const randomNumber = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
@@ -55,6 +109,24 @@ const randomColor = () =>
   )})`;
 
 document.querySelector('.nav__link').addEventListener('click', function (e) {
-  console.log(e.currentTarget);
-  e.currentTarget.style.backgroundColor = randomColor();
+  this.style.backgroundColor = randomColor();
+  e.stopPropagation();
 });
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+*/
+const h1 = document.querySelector('h1');
+
+console.log(h1.querySelectorAll('span'));
+
+console.log(h1.childNodes);
+console.log(h1.children);
+
+console.log(h1.parentElement);
+console.log(h1.closest);
+
+console.log(h1.previousSibling);
